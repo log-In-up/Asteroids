4e810b237eda9bf02a6d1c2d9dee061c000a6ab5
Ruslan Vyasel'kin <rus123rus98@gmail.com>
2022-12-26 01:00:57 +0300

    Unit test fix.

--- Assets/Scripts/UnitTests/Play Mode Tests/UserInterfaceTests.cs
+++ Assets/Scripts/UnitTests/Play Mode Tests/UserInterfaceTests.cs
@@ -1,28 +1,20 @@
+using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
-using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
-using UnityEngine.TestTools;
 using UnityEngine.ResourceManagement.AsyncOperations;
-using System.Text;
-using System.Linq;
+using UnityEngine.TestTools;
 using UserInterface;
+using UserInterface.Screens;
 
 public class UserInterfaceTests
 {
     #region Fields
-    private GameObject _userInterface = null;
-
     private AsyncOperationHandle<GameObject> handleLoadUserInterface;
 
     private const string USER_INTERFACE = "User Interface",
-        SCREEN_CONTENT = "User Interface(Clone)/Screen Content",
-        BATTLE_RESULT = "Battle Result Screen", BATTLE = "Battle Screen",
-        CHARACTERISTICS = "Characteristics Screen", GAME_LOADING = "Game Loading Screen",
-        INVENTORY = "Inventory Screen", ITEM_STAT = "Item Stat Screen",
-        LEVELS = "Levels Screen", MAIN_MENU = "Main Menu Screen",
-        SETTINGS = "Settings Screen", SPLASH = "Splash Screen", STORE = "Store Screen";
+        SCREEN_CONTENT = "User Interface(Clone)/Screen Content";
     #endregion
 
     #region Test setup
@@ -30,14 +22,12 @@ public class UserInterfaceTests
     public void Setup()
     {
         handleLoadUserInterface = Addressables.LoadAssetAsync<GameObject>(USER_INTERFACE);
-
-        handleLoadUserInterface.Completed += OnCompleteLoadUserInterface;
     }
 
     [TearDown]
     public void Teardown()
     {
-        handleLoadUserInterface.Completed -= OnCompleteLoadUserInterface;
+
     }
     #endregion
 
@@ -53,33 +43,26 @@ public class UserInterfaceTests
             yield return null;
         }
 
-        GameObject ui = Object.Instantiate(_userInterface);
+        GameObject ui = Object.Instantiate(handleLoadUserInterface.Result);
 
         UICore uiCore = ui.GetComponent<UICore>();
         uiCore.OpenScreen(UIScreen.BattleScreen);
 
         GameObject uiCoreObj = GameObject.Find(SCREEN_CONTENT);
-        RectTransform[] screens = uiCoreObj.GetComponentsInChildren<RectTransform>(true);
+        ScreenObserver[] screens = uiCoreObj.GetComponentsInChildren<ScreenObserver>(true);
 
         List<bool> actual = new List<bool>();
-        foreach (RectTransform screen in screens)
+        foreach (ScreenObserver screen in screens)
         {
             actual.Add(screen.gameObject.activeInHierarchy);
         }
 
         List<bool> expected = new List<bool>()
         { 
-            true, false, true, false, false, false, false, false, false, false, false, false
+            false, true, false, false, false, false, false, false, false, false, false
         };
 
         Assert.AreEqual(expected, actual);
-    }
-    #endregion
-
-    #region Event methods
-    private void OnCompleteLoadUserInterface(AsyncOperationHandle<GameObject> loadedObject)
-    {
-        _userInterface = loadedObject.Result;
     }
     #endregion
 }
